17788775193b93ea002bf2b4eeb22223bf7668df
pinnaka99 <40848579+pinnaka99@users.noreply.github.com>
2019-07-27 22:53:11 +0530

    homepage

---
+++ homepage.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+	<title>Index</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="background.css">
+</head>
+<body>
+	     <div class="container-fluid headpart">
+  <div class="starting" style="padding:0px;">
+   
+     <div id="logo">
+          <img src="pvplogo.jpg" width="120" height="120" >
+        </div>
+      
+
+     <div class="starting" style="padding:0px;">
+       <h1 style="margin-left:20%">Prasad V. Potluri Siddhartha Institute of Technology</h1>
+      <h3 style="margin-left:38%"><font color='#0F2AF1'>Accredited with  'A+' grade  by NAAC </font></h3>
+          
+          <h3 style="margin-left:18%">AUTONOMOUS, ISO 9001:2015 Certified Institute,  Permanently Affiliated to JNTUK, KAKINADA</h4>
+          
+          <h5 style="margin-left:35%"><b>All UG Programmes are provisionally accredited by NBA</h5>
+
+
+     </div> 
+
+  </div>
+</div>
+
+<div class="navbar">
+	<a class="active" href="#home">HOME</a>
+	<div class="dropdown">
+	<button class="dropbtn">STAFF-LOGIN<i class="fa fa-caret-down"></i>
+    </button>
+
+    <div class="dropdown-content">
+    <a href="emplogin.php">TEACHING</a>
+    <a href="emplogin.php">NON-TEACHING</a>
+
+  </div>
+</div>
+<a href="admin.php">ADMIN-LOGIN</a>
+		
+		<a href="hodlogin.php">HOD-LOGIN</a>
+
+</div>
+<marquee id="m">/*latest news displacement*/</marquee>
+<div id="image">
+	<div class="item active">
+        <img src="home_buddha.jpg" alt="Flower" width="100%" height="100%">
+       <center> <div class="centered" style="width:50%;height:40%;"><h1>Employee Leave Management System</h1></div></center>
+</div>
+</div>
+</body>
+
+</html>
